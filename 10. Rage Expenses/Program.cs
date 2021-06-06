b6bac739f249c89d11c55af71067b3c5696613b1
ADSirakova93 <adsirakova.93@gmail.com>
2021-06-06 10:43:43 +0300

    Add project files.

---
+++ 10. Rage Expenses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGame = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+
+            double crashHeadset = lostGame / 2;
+            double crashMouse = lostGame / 3;
+            double crashKeyboard = lostGame / 6;
+            double crashDisplay = lostGame / 12;
+
+            double totalExpenses = crashHeadset * headsetPrice + crashMouse * mousePrice + crashKeyboard * keyboardPrice + crashDisplay * displayPrice;
+            Console.WriteLine($"Rage expenses: {totalExpenses:f2} lv.");
+        }
+    }
+}
---
+++ 9. Padawan EquipmentSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _9._Padawan_EquipmentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyStart = double.Parse(Console.ReadLine());
+            double students = double.Parse(Console.ReadLine());
+            double priceSabers = double.Parse(Console.ReadLine());
+            double robes = double.Parse(Console.ReadLine());
+            double belts = double.Parse(Console.ReadLine());
+            double saberextra = Math.Ceiling(0.10 * students);
+            double freeBelts = Math.Floor(students / 6);
+
+            double needMoney = (priceSabers * (students + saberextra) + (robes * students) + (belts * (students - freeBelts)));
+
+            if (needMoney <= moneyStart)
+            {
+                Console.WriteLine($"The money is enough - it would cost {needMoney:f2}lv.");
+            }
+            else
+            {
+                double diff = needMoney - moneyStart;
+                Console.WriteLine($"Ivan Cho will need {diff:f2}lv more.");
+            }
+        }
+    }
+}
